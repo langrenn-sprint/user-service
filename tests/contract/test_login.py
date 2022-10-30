29f0f578fb7ed78d6a398be8acd1aa7c2ca057e5
Stig B. Dørmænen <stigbd@gmail.com>
2022-10-30 16:38:12 +0100

    chore: fixup tests and code

--- tests/contract/test_login.py
+++ tests/contract/test_login.py
@@ -23,3 +23,23 @@ async def test_login(http_service: Any) -> None:
 
     assert response.status == 200
     assert body["token"]
+
+
+@pytest.mark.contract
+@pytest.mark.asyncio
+async def test_login_wrong_password(http_service: Any) -> None:
+    """Should return 401."""
+    url = f"{http_service}/login"
+    headers = {hdrs.CONTENT_TYPE: "application/json"}
+    request_body = {
+        "username": os.getenv("ADMIN_USERNAME"),
+        "password": "WRONG_PASSWORD",
+    }
+    session = ClientSession()
+    async with session.post(url, headers=headers, json=request_body) as response:
+        body = await response.json()
+    await session.close()
+
+    assert response.status == 401
+    assert response.content_type == "application/json"
+    assert body["detail"] == "Incorrect username or password"
